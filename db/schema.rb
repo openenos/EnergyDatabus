82c040298a322f85ee8eeb5b1d23c8f99feb2dfd
sridhar61 <sridhar.sidhu61@gmail.com>
2015-01-14 12:11:40 +0530

    Sridhar - devise user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113144500) do
+ActiveRecord::Schema.define(version: 20150114053903) do
 
   create_table "api_keys", force: true do |t|
     t.string   "access_token"
@@ -113,6 +113,24 @@ ActiveRecord::Schema.define(version: 20150113144500) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "utilities", force: true do |t|
     t.string   "display"
