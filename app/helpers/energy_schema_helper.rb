c21ea91e8c383edd793f7fe17c1f4f2e8ad44667
sridhar61 <sridhar.sidhu61@gmail.com>
2015-01-20 10:50:40 +0530

    Sridhar - Workers

--- app/helpers/energy_schema_helper.rb
+++ app/helpers/energy_schema_helper.rb
@@ -12,19 +12,19 @@ cluster = Cassandra.cluster
 session  = cluster.connect()
 
 # Creating a simple keyspace with replication factor 1
-session.execute_async("CREATE KEYSPACE enos_#{name} WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }")
+session.execute("CREATE KEYSPACE enos_#{name} WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }")
 
-session.execute_async("USE enos_#{name}")
+session.execute("USE enos_#{name}")
 # Creating a table
 # panel or site_ref both are same in db perspective
 
-session.execute_async("CREATE TABLE emon_min_by_data(panel text, channel text, asof_min int, value int, PRIMARY KEY((panel,channel),asof_min)) WITH COMPACT STORAGE AND CLUSTERING ORDER BY(asof_min desc)")
+session.execute("CREATE TABLE emon_min_by_data(panel text, channel text, asof_min int, value int, PRIMARY KEY((panel,channel),asof_min)) WITH COMPACT STORAGE AND CLUSTERING ORDER BY(asof_min desc)")
 
-session.execute_async("CREATE TABLE emon_hourly_data(panel text, channel text, asof_hr int, value int, PRIMARY KEY((panel,channel),asof_hr)) WITH COMPACT STORAGE AND CLUSTERING ORDER BY(asof_hr desc)")
+session.execute("CREATE TABLE emon_hourly_data(panel text, channel text, asof_hr int, value int, PRIMARY KEY((panel,channel),asof_hr)) WITH COMPACT STORAGE AND CLUSTERING ORDER BY(asof_hr desc)")
 
-session.execute_async("CREATE TABLE emon_hourly_runtime(panel text, channel text, asof_hr int, value int, PRIMARY KEY((panel,channel),asof_hr)) WITH COMPACT STORAGE AND CLUSTERING ORDER BY(asof_hr desc)")
+session.execute("CREATE TABLE emon_hourly_runtime(panel text, channel text, asof_hr int, value int, PRIMARY KEY((panel,channel),asof_hr)) WITH COMPACT STORAGE AND CLUSTERING ORDER BY(asof_hr desc)")
 
-session.execute_async("CREATE TABLE weather_min_by_data(station text, asof_min int, value float, PRIMARY KEY(station, asof_min)) WITH COMPACT STORAGE AND CLUSTERING ORDER BY(asof_min desc)")
+session.execute("CREATE TABLE weather_min_by_data(station text, asof_min int, value float, PRIMARY KEY(station, asof_min)) WITH COMPACT STORAGE AND CLUSTERING ORDER BY(asof_min desc)")
 
 #db.execute("CREATE TABLE emon_daily_runtime(panel text, channel text, asof_day bigint, value int, PRIMARY KEY((panel,channel),asof_day)) WITH COMPACT STORAGE AND CLUSTERING ORDER BY(asof_day desc)")
 
