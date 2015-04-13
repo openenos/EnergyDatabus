098158b30aa44df696b798258a714fe1cc64e1dd
ARahaamn <argoodfrnd2u@gmail.com>
2015-04-13 12:36:44 +0530

    Removed child directory

--- eNosServer/src/config/kairosdb.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var kairosdb = require('kairosdb'),
-    config = require("./config"),
-    winston = require("./logs"),
-    kairosdbConnection = kairosdb.init(config.kairosdb_host, config.kairosdb_port, {});
-    
-    module.exports = kairosdbConnection;
--- eNosServer/src/config/logs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var winston = require('winston'),
-    config = require("./config");
-    winston.add(winston.transports.File, { filename: config.log_file });
-    winston.remove(winston.transports.Console);
-
-module.exports = winston;
